361a9f0e4a1493afcdebb9b005f040e7e7e0017a
sqrfactor <agnimgupta11@gmail.com>
2018-04-17 10:39:02 +0530

    Bootstrap and jquery files!

--- resources/views/sqrfactor/partials/footer-script.blade.php
+++ resources/views/sqrfactor/partials/footer-script.blade.php
@@ -1,8 +1,14 @@
 <!-- jquery -->
-<script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
+<!-- <script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-migrate-1.4.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-migrate-3.0.0.min.js"></script>
+
 <!-- Bootstrap script -->
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src="{{ asset('vendor/tether/dist/js/tether.min.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<!-- <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script> -->
 <!-- Js effects for material design. + Tooltips -->
 <script src="{{ asset('assets/js/material.min.js') }}"></script>
 <!-- Helper scripts (Tabs, Equal height, Scrollbar, etc) -->
